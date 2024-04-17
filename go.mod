af0785e579f7561eb48572f8193744a45ff3017e
Smith, Jacob Tyler <jts11@iu.edu>
2024-04-17 11:33:19 -0400

    4/17

--- go.mod
+++
@@ -1,3 +0,0 @@
-module arithmetic-service-jts2052.onrender.com
-
-go 1.22.0
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
 app.use(cors());
-const port = 3000;
+
+if (!process.env.PORT) {
+    console.log('PORT is not defined');
+    process.exit(1);
+}
+
+const port = process.env.PORT;
 
 app.get('/', (req, res) => res.send('Arithmetic service - Hello World!'));
 
